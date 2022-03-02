d96f0bce7c9cf3881a7dfac265d0c88ce8c9f5ed
adri <19190248+adrifdez@users.noreply.github.com>
2022-03-02 12:02:04 +0100

    Convert component to function

--- src/views/app.view.tsx
+++ src/views/app.view.tsx
@@ -1,18 +1,14 @@
-import { Component } from "react";
+import "src/views/app.styles.ts";
 
-import "src/views/app.style.ts";
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to Bridge</h2>
-        </div>
-        <p className="App-intro">Polygon Hermez Bridge</p>
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Welcome to Bridge</h2>
       </div>
-    );
-  }
+      <p className="App-intro">Polygon Hermez Bridge</p>
+    </div>
+  );
 }
 
 export default App;
