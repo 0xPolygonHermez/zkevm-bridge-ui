5e319355a423ba17ff328a19e9bab17ba527a563
adri <19190248+adrifdez@users.noreply.github.com>
2022-03-04 16:38:36 +0100

    Add Icon suffix to icons

--- src/views/home/components/header/header.view.tsx
+++ src/views/home/components/header/header.view.tsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 
 import { ReactComponent as Logo } from "src/assets/hermez-logo.svg";
-import { ReactComponent as Settings } from "src/assets/icons/settings.svg";
-import { ReactComponent as QR } from "src/assets/icons/qr.svg";
-import useHeaderHomeStyles from "src/views/home/components/header/header.styles";
+import { ReactComponent as SettingsIcon } from "src/assets/icons/settings.svg";
+import { ReactComponent as QRIcon } from "src/assets/icons/qr.svg";
+import useHeaderStyles from "src/views/home/components/header/header.styles";
 
 const Header = () => {
-  const classes = useHeaderHomeStyles();
+  const classes = useHeaderStyles();
   return (
     <header className={classes.header}>
       <div className={classes.settingsButton}>
         <Link to={"/"} className={classes.button}>
-          <Settings className={classes.icon} />
+          <SettingsIcon className={classes.icon} />
           <p className={classes.text}>Settings</p>
         </Link>
       </div>
@@ -22,7 +22,7 @@ const Header = () => {
       <div className={classes.myAccountButton}>
         <Link to={"/"} className={classes.button}>
           <p className={classes.text}>My Address</p>
-          <QR className={classes.icon} />
+          <QRIcon className={classes.icon} />
         </Link>
       </div>
     </header>
