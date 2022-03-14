b761c41763e875645677b3e2194ffd771e6504dd
adri <19190248+adrifdez@users.noreply.github.com>
2022-03-14 11:22:43 +0100

    Add breakline

--- src/utils/amounts.ts
+++ src/utils/amounts.ts
@@ -7,6 +7,7 @@ const rates = {
   dai: 1.02,
   eth: 2500,
 };
+
 export const fiatAmount = ({ amount, token }: FiatAmountProps): string => {
   const fiat = amount * rates[token];
   return `$${fiat.toFixed(2)}`;
