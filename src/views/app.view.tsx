6e74a8ebd15021ac8b3e75bbf85017023420ff35
Alex Monso <amonsosanz@gmail.com>
2022-03-17 13:28:58 +0100

    Add a global context and implement the snackbar state and callbacks

--- src/views/app.view.tsx
+++ src/views/app.view.tsx
@@ -2,19 +2,22 @@ import useAppStyles from "src/views/app.styles";
 import Layout from "src/views/core/layout/layout.view";
 import { EthereumProviderProvider } from "src/contexts/ethereum-provider.context";
 import { EnvProvider } from "src/contexts/env.context";
+import { GlobalProvider } from "src/contexts/global.context";
 import Router from "./core/router/router.view";
 
 const App = (): JSX.Element => {
   useAppStyles();
 
   return (
-    <EnvProvider>
-      <EthereumProviderProvider>
-        <Layout>
-          <Router />
-        </Layout>
-      </EthereumProviderProvider>
-    </EnvProvider>
+    <GlobalProvider>
+      <EnvProvider>
+        <EthereumProviderProvider>
+          <Layout>
+            <Router />
+          </Layout>
+        </EthereumProviderProvider>
+      </EnvProvider>
+    </GlobalProvider>
   );
 };
 
