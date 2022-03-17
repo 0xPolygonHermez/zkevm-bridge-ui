28625f42c152bcb2439e3372b3b5431d25e5a458
Elias Garcia <eliasgarciadev@gmail.com>
2022-03-17 12:31:53 +0100

    Improve AccountLoader view

--- src/views/login/components/account-loader/account-loader.view.tsx
+++ src/views/login/components/account-loader/account-loader.view.tsx
@@ -5,6 +5,11 @@ import { WalletName } from "src/domain";
 import Typography from "src/views/shared/typography/typography.view";
 import WalletIcon from "../wallet-icon/wallet-icon.view";
 
+const walletLabels = {
+  [WalletName.METAMASK]: "MetaMask",
+  [WalletName.WALLET_CONNECT]: "WalletConnect",
+};
+
 interface AccountLoaderProps {
   selectedWallet: WalletName;
 }
@@ -12,38 +17,17 @@ interface AccountLoaderProps {
 const AccountLoader: FC<AccountLoaderProps> = ({ selectedWallet }) => {
   const classes = useAccountLoaderStyles();
 
-  switch (selectedWallet) {
-    case WalletName.METAMASK: {
-      return (
-        <div className={classes.accountLoader}>
-          <WalletIcon walletName={WalletName.METAMASK} size="lg" className={classes.walletIcon} />
-          <Typography type="body1" className={classes.walletName}>
-            MetaMask
-          </Typography>
-          <Typography type="body2" className={classes.hint}>
-            Follow the instructions in MetaMask
-          </Typography>
-        </div>
-      );
-    }
-    case WalletName.WALLET_CONNECT: {
-      return (
-        <div className={classes.accountLoader}>
-          <WalletIcon
-            walletName={WalletName.WALLET_CONNECT}
-            size="lg"
-            className={classes.walletIcon}
-          />
-          <Typography type="body1" className={classes.walletName}>
-            WalletConnect
-          </Typography>
-          <Typography type="body2" className={classes.hint}>
-            Follow the instructions in WalletConnect
-          </Typography>
-        </div>
-      );
-    }
-  }
+  return (
+    <div className={classes.accountLoader}>
+      <WalletIcon walletName={selectedWallet} size="lg" className={classes.walletIcon} />
+      <Typography type="body1" className={classes.walletName}>
+        {walletLabels[selectedWallet]}
+      </Typography>
+      <Typography type="body2" className={classes.hint}>
+        Follow the instructions in {walletLabels[selectedWallet]}
+      </Typography>
+    </div>
+  );
 };
 
 export default AccountLoader;
