97fc725d1cb6f27ce5b21cb73ef791e5c7d91fe5
Alex Monso <amonsosanz@gmail.com>
2022-03-17 13:54:41 +0100

    Show the snackbar from the layout view

--- src/views/core/layout/layout.view.tsx
+++ src/views/core/layout/layout.view.tsx
@@ -1,14 +1,30 @@
 import { FC } from "react";
 
 import useLayoutStyles from "src/views/core/layout/layout.styles";
+import Snackbar from "src/views/shared/snackbar/snackbar.view";
+import { useGlobalContext } from "src/contexts/global.context";
+import { reportError } from "src/adapters/error";
 
 const Layout: FC = ({ children }) => {
   const classes = useLayoutStyles();
+  const { snackbar, closeSnackbar } = useGlobalContext();
+
+  const onCloseSnackbar = closeSnackbar;
+  const onReportFromSnackbar = reportError;
 
   return (
-    <div className={classes.layout}>
-      <div className={classes.container}>{children}</div>
-    </div>
+    <>
+      <div className={classes.layout}>
+        <div className={classes.container}>{children}</div>
+      </div>
+      {snackbar.status === "open" && (
+        <Snackbar
+          message={snackbar.message}
+          onClose={onCloseSnackbar}
+          onReport={onReportFromSnackbar}
+        />
+      )}
+    </>
   );
 };
 
