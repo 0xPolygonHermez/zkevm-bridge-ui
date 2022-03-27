60a3eae695c256907fd0e8a308b94e228039a861
Alex Monso <amonsosanz@gmail.com>
2022-03-27 20:56:55 +0200

    Fix compilation error due to wrong import

--- scripts/generate-proto-types.sh
+++ scripts/generate-proto-types.sh
@@ -4,20 +4,25 @@
 # installed in your system, along with an env var GH_TOKEN storing a valid
 # GitHub token to grant access to https://github.com/hermeznetwork/comms-protocol
 
-TMP_PATH="tmp"
-TARGET_PATH="src/types"
-PROTO_PATH="$TMP_PATH/third_party"
-PROTO_FILE="proto/bridge/v1/query.proto"
+BASE_PATH="tmp"
+IMPORTS_PATH="$BASE_PATH/third_party"
+PROTO_PATH="proto/bridge/v1"
+PROTO_FILE="query.proto"
+OUTPUT_PATH="src/types"
 REPOSITORY="https://$GH_TOKEN@github.com/hermeznetwork/comms-protocol.git"
 BRANCH="feature/bridge"
 
-mkdir $TMP_PATH
-mkdir $TARGET_PATH
-git clone --branch $BRANCH $REPOSITORY $TMP_PATH
+mkdir $BASE_PATH
+mkdir $OUTPUT_PATH
+git clone --branch $BRANCH $REPOSITORY $BASE_PATH
 
-protoc -I=$TMP_PATH $PROTO_FILE \
-  --proto_path=.:$PROTO_PATH \
-  --js_out=import_style=commonjs:$TARGET_PATH \
-  --grpc-web_out=import_style=typescript,mode=grpcwebtext:$TARGET_PATH
+protoc -I=$BASE_PATH $PROTO_PATH/$PROTO_FILE \
+  --proto_path=$IMPORTS_PATH \
+  --js_out=import_style=commonjs:$OUTPUT_PATH \
+  --grpc-web_out=import_style=typescript,mode=grpcwebtext:$OUTPUT_PATH
 
-rm -rf $TMP_PATH
+# Remove unused import that breaks the compilation until this is fixed:
+# https://github.com/grpc/grpc-web/issues/529
+ls -1 $OUTPUT_PATH/$PROTO_PATH/*.js | xargs sed -i '/google_api_annotations_pb =/d'
+
+rm -rf $BASE_PATH
