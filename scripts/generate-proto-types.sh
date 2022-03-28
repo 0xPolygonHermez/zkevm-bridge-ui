156a5d415903e7f65631deab3d7b8a26ed849225
Alex Monso <amonsosanz@gmail.com>
2022-03-28 11:43:40 +0200

    Rename BASE_PATH as TMP_PATH

--- scripts/generate-proto-types.sh
+++ scripts/generate-proto-types.sh
@@ -4,19 +4,19 @@
 # installed in your system, along with an env var GH_TOKEN storing a valid
 # GitHub token to grant access to https://github.com/hermeznetwork/comms-protocol
 
-BASE_PATH="tmp"
-IMPORTS_PATH="$BASE_PATH/third_party"
+TMP_PATH="tmp"
+IMPORTS_PATH="$TMP_PATH/third_party"
 PROTO_PATH="proto/bridge/v1"
 PROTO_FILE="query.proto"
 OUTPUT_PATH="src/types"
 REPOSITORY="https://$GH_TOKEN@github.com/hermeznetwork/comms-protocol.git"
 BRANCH="feature/bridge"
 
-mkdir $BASE_PATH
+mkdir $TMP_PATH
 mkdir $OUTPUT_PATH
-git clone --branch $BRANCH $REPOSITORY $BASE_PATH
+git clone --branch $BRANCH $REPOSITORY $TMP_PATH
 
-protoc -I=$BASE_PATH $PROTO_PATH/$PROTO_FILE \
+protoc -I=$TMP_PATH $PROTO_PATH/$PROTO_FILE \
   --proto_path=$IMPORTS_PATH \
   --js_out=import_style=commonjs:$OUTPUT_PATH \
   --grpc-web_out=import_style=typescript,mode=grpcwebtext:$OUTPUT_PATH
@@ -25,4 +25,4 @@ protoc -I=$BASE_PATH $PROTO_PATH/$PROTO_FILE \
 # https://github.com/grpc/grpc-web/issues/529
 ls -1 $OUTPUT_PATH/$PROTO_PATH/*.js | xargs sed -i '/google_api_annotations_pb =/d'
 
-rm -rf $BASE_PATH
+rm -rf $TMP_PATH
