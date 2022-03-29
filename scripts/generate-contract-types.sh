36dddc6145036088cbeae11a849d456c4aaaf030
Alex Monso <amonsosanz@gmail.com>
2022-03-29 16:22:44 +0200

    Generate the types for the contract of the Bridge

--- scripts/generate-contract-types.sh
+++ scripts/generate-contract-types.sh
@@ -2,3 +2,6 @@
 
 # Generate Price Oracle contract types
 npx typechain --target ethers-v5 --out-dir src/types/contracts/price-oracle "abis/price-oracle.json"
+
+# Generate Bridge contract types
+npx typechain --target ethers-v5 --out-dir src/types/contracts/bridge "abis/bridge.json"
