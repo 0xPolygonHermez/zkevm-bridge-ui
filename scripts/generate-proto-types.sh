9369c788b7f20ce5c04a75df4a98dd4ecc7e0348
Alex Monso <amonsosanz@gmail.com>
2022-03-31 11:50:54 +0200

    Add argument to choose among -ssh and -https to clone the comms-protocol repository from generate-proto-types.sh

--- scripts/generate-proto-types.sh
+++ scripts/generate-proto-types.sh
@@ -2,7 +2,7 @@
 
 # Making sure "protoc" is available and exiting otherwise
 if ! command -v protoc &>/dev/null; then
-cat <<EOF
+  cat <<EOF
 gRPC compiler (protoc) could not be found on your system.
 
 Please visit https://grpc.io/docs/protoc-installation/ for
@@ -16,7 +16,7 @@ fi
 
 # Making sure "protoc-gen-grpc-web" is available and exiting otherwise
 if ! command -v protoc-gen-grpc-web &>/dev/null; then
-cat <<EOF
+  cat <<EOF
 gRPC Web code generator plugin (protoc-gen-grpc-web) could
 not be found on your system.
 
@@ -31,7 +31,7 @@ fi
 
 # Making sure "git" is available and exiting otherwise
 if ! command -v git &>/dev/null; then
-cat <<EOF
+  cat <<EOF
 Git (git) could not be found on your system.
 
 Please visit https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
@@ -43,13 +43,20 @@ EOF
   exit 1
 fi
 
-# Making sure "GH_TOKEN" env var is available and exiting otherwise
-if [ -z ${GH_TOKEN+x} ]; then
-cat <<EOF
-The env var GH_TOKEN could not be found on your system.
+TMP_PATH="tmp"
+IMPORTS_PATH="$TMP_PATH/third_party"
+PROTO_PATH="proto/bridge/v1"
+PROTO_FILE="query.proto"
+OUTPUT_PATH="src/types"
+SSH_REPOSITORY="git@github.com:hermeznetwork/comms-protocol.git"
+HTTP_REPOSITORY="https://$GH_TOKEN@github.com/hermeznetwork/comms-protocol.git"
+BRANCH="main"
 
-An env var GH_TOKEN storing a valid GitHub token to grant access to
-https://github.com/hermeznetwork/comms-protocol is required.
+# Checking for ssh/https argument
+if [ $# -ne 1 ]; then
+  cat <<EOF
+Please pass the argument -ssh or -https to specify which protocol should I use
+to clone the repository https://github.com/hermeznetwork/comms-protocol
 
 Exiting now :(
 
@@ -57,17 +64,44 @@ EOF
   exit 1
 fi
 
-# All dependencies are available, we can move on!
-TMP_PATH="tmp"
-IMPORTS_PATH="$TMP_PATH/third_party"
-PROTO_PATH="proto/bridge/v1"
-PROTO_FILE="query.proto"
-OUTPUT_PATH="src/types"
-REPOSITORY="https://$GH_TOKEN@github.com/hermeznetwork/comms-protocol.git"
-BRANCH="main"
+if [ "$1" == "-ssh" ]; then
+  git clone --branch $BRANCH $SSH_REPOSITORY $TMP_PATH
+elif [ "$1" == "-https" ]; then
 
-git clone --branch $BRANCH $REPOSITORY $TMP_PATH
+  # Making sure "GH_TOKEN" env var is available and exiting otherwise
+  if [ -z ${GH_TOKEN+x} ]; then
+    cat <<EOF
+The env var GH_TOKEN could not be found on your system.
 
+An env var GH_TOKEN storing a valid GitHub token is required to grant access to
+https://github.com/hermeznetwork/comms-protocol using the -https protocol.
+
+Exiting now :(
+
+EOF
+    exit 1
+  fi
+
+  git clone --branch $BRANCH $HTTP_REPOSITORY $TMP_PATH
+
+else
+  cat <<EOF
+The provided argument $1 is invalid.
+
+Please pass the argument -ssh or -https to specify which protocol should I use
+to clone the repository https://github.com/hermeznetwork/comms-protocol
+
+Exiting now :(
+
+EOF
+  exit 1
+fi
+
+if [ ! -d $OUTPUT_PATH ]; then
+  mkdir $OUTPUT_PATH
+fi
+
+# All options and dependencies are available, we can move on!
 protoc -I=$TMP_PATH $PROTO_PATH/$PROTO_FILE \
   --proto_path=$IMPORTS_PATH \
   --js_out=import_style=commonjs:$OUTPUT_PATH \
