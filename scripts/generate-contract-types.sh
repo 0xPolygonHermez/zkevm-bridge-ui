1209abdb27ac22da4fec7c5732afb7df3a3c9958
Alex Monso <amonsosanz@gmail.com>
2022-04-01 10:25:13 +0200

    Generate types for the contract global-exit-root-manager

--- scripts/generate-contract-types.sh
+++ scripts/generate-contract-types.sh
@@ -1,7 +1,13 @@
 #/bin/bash
 
+BASE_PATH="src/types/contracts"
+
+# Clean up
+rm -rf BASE_PATH
+
 # Generate Price Oracle contract types
-npx typechain --target ethers-v5 --out-dir src/types/contracts/uniswap-quoter "abis/uniswap-quoter.json"
+npx typechain --target ethers-v5 --out-dir $BASE_PATH/uniswap-quoter "abis/uniswap-quoter.json"
 
 # Generate Bridge contract types
-npx typechain --target ethers-v5 --out-dir src/types/contracts/bridge "abis/bridge.json"
+npx typechain --target ethers-v5 --out-dir $BASE_PATH/bridge/bridge "abis/bridge/bridge.json"
+npx typechain --target ethers-v5 --out-dir $BASE_PATH/bridge/global-exit-root-manager "abis/bridge/global-exit-root-manager.json"
