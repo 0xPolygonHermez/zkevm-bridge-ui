d05cbbbca92711e67b8ceeb38b887cc8a2d29e6c
Elias Garcia <eliasgarciadev@gmail.com>
2022-04-04 18:47:30 +0200

    Remove testing code

--- src/views/home/home.view.tsx
+++ src/views/home/home.view.tsx
@@ -4,19 +4,10 @@ import Header from "src/views/home/components/header/header.view";
 import Typography from "src/views/shared/typography/typography.view";
 import { getPartiallyHiddenEthereumAddress } from "src/utils/addresses";
 import { useProvidersContext } from "src/contexts/providers.context";
-import { useBridgeContext } from "src/contexts/bridge.context";
-import { useEffect } from "react";
 
 const Home = (): JSX.Element => {
   const classes = useHomeStyles();
   const { account } = useProvidersContext();
-  const { getBridges } = useBridgeContext();
-
-  useEffect(() => {
-    getBridges({ ethereumAddress: "0xeB17ce701E9D92724AA2ABAdA7E4B28830597Dd9" })
-      .then(console.log)
-      .catch(console.error);
-  }, [getBridges]);
 
   return (
     <>
