9675d158ad2774a47e9f9e4374f5dbd1dc8db728
Alex Monso <amonsosanz@gmail.com>
2022-04-04 16:53:56 +0200

    Memoize the callbacks bridge and claim

--- src/contexts/bridge.context.tsx
+++ src/contexts/bridge.context.tsx
@@ -1,5 +1,5 @@
 import { BigNumber, ContractTransaction, PayableOverrides } from "ethers";
-import { createContext, FC, useContext, useEffect, useState } from "react";
+import { createContext, FC, useContext, useEffect, useState, useCallback } from "react";
 
 import { useEnvContext } from "src/contexts/env.context";
 import { useProvidersContext } from "src/contexts/providers.context";
@@ -42,60 +42,72 @@ const BridgeProvider: FC = (props) => {
   const { connectedProvider } = useProvidersContext();
   const [bridgeContract, setBridgeContract] = useState<Bridge>();
 
-  const bridge = (
-    token: string,
-    amount: BigNumber,
-    destinationNetwork: number,
-    destinationAddress: string,
-    overrides?: PayableOverrides & { from?: string | Promise<string> }
-  ): Promise<ContractTransaction> => {
-    if (env === undefined) {
-      throw new Error("Environment is not available");
-    }
+  const bridge = useCallback(
+    (
+      token: string,
+      amount: BigNumber,
+      destinationNetwork: number,
+      destinationAddress: string,
+      overrides?: PayableOverrides & { from?: string | Promise<string> }
+    ): Promise<ContractTransaction> => {
+      if (env === undefined) {
+        throw new Error("Environment is not available");
+      }
 
-    if (bridgeContract === undefined) {
-      throw new Error("Bridge contract is not available");
-    }
+      if (bridgeContract === undefined) {
+        throw new Error("Bridge contract is not available");
+      }
 
-    return bridgeContract.bridge(token, amount, destinationNetwork, destinationAddress, overrides);
-  };
+      return bridgeContract.bridge(
+        token,
+        amount,
+        destinationNetwork,
+        destinationAddress,
+        overrides
+      );
+    },
+    [bridgeContract, env]
+  );
 
-  const claim = async (
-    originalTokenAddress: string,
-    amount: BigNumber,
-    originalNetwork: string,
-    destinationNetwork: number,
-    destinationAddress: string,
-    smtProof: string[],
-    globalExitRootNum: number,
-    mainnetExitRoot: string,
-    rollupExitRoot: string,
-  ): Promise<ContractTransaction> => {
-    if (env === undefined) {
-      throw new Error("Environment is not available");
-    }
+  const claim = useCallback(
+    async (
+      originalTokenAddress: string,
+      amount: BigNumber,
+      originalNetwork: string,
+      destinationNetwork: number,
+      destinationAddress: string,
+      smtProof: string[],
+      globalExitRootNum: number,
+      mainnetExitRoot: string,
+      rollupExitRoot: string
+    ): Promise<ContractTransaction> => {
+      if (env === undefined) {
+        throw new Error("Environment is not available");
+      }
 
-    if (bridgeContract === undefined) {
-      throw new Error("Bridge contract is not available");
-    }
+      if (bridgeContract === undefined) {
+        throw new Error("Bridge contract is not available");
+      }
 
-    const filter = bridgeContract.filters.ClaimEvent(null, null, null, null, null);
-    const events = await bridgeContract.queryFilter(filter, 0, "latest");
-    const index = events.length;
+      const filter = bridgeContract.filters.ClaimEvent(null, null, null, null, null);
+      const events = await bridgeContract.queryFilter(filter, 0, "latest");
+      const index = events.length;
 
-    return bridgeContract.claim(
-      originalTokenAddress,
-      amount,
-      originalNetwork,
-      destinationNetwork,
-      destinationAddress,
-      smtProof,
-      index,
-      globalExitRootNum,
-      mainnetExitRoot,
-      rollupExitRoot
-    );
-  };
+      return bridgeContract.claim(
+        originalTokenAddress,
+        amount,
+        originalNetwork,
+        destinationNetwork,
+        destinationAddress,
+        smtProof,
+        index,
+        globalExitRootNum,
+        mainnetExitRoot,
+        rollupExitRoot
+      );
+    },
+    [bridgeContract, env]
+  );
 
   useEffect(() => {
     if (env && connectedProvider) {
