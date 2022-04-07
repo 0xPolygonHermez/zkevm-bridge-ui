e8d5033f7e78cac3038e704c685d56ec509e407e
adri <19190248+adrifdez@users.noreply.github.com>
2022-04-07 10:55:42 +0200

    Improve transaction context

--- src/contexts/transaction.context.tsx
+++ src/contexts/transaction.context.tsx
@@ -1,15 +1,8 @@
-import { createContext, FC, useCallback, useContext, useMemo, useState } from "react";
+import { createContext, FC, useContext, useMemo, useState } from "react";
 
-import { chains } from "src/assets/chains/chains";
+import { chains } from "src/constants";
 import tokens from "src/assets/tokens/tokens.json";
-import { Chain, Token } from "src/domain";
-
-type Transaction = {
-  chainFrom: Chain;
-  chainTo: Chain;
-  token: Token;
-  amount: number;
-};
+import { Transaction } from "src/domain";
 
 interface TransactionContext {
   transaction: Transaction;
@@ -19,30 +12,20 @@ interface TransactionContext {
 const transactionContextDefaultValue: TransactionContext = {
   transaction: { chainFrom: chains[0], chainTo: chains[1], token: tokens[0], amount: 0 },
   setTransaction: () => {
-    console.log("The transaction context is not yet ready");
+    console.error("The transaction context is not yet ready");
   },
 };
 
 const transactionContext = createContext<TransactionContext>(transactionContextDefaultValue);
 
 const TransactionProvider: FC = (props) => {
-  const [chainFrom, setChainFrom] = useState<Chain>(
-    transactionContextDefaultValue.transaction.chainFrom
+  const [transaction, setTransaction] = useState<Transaction>(
+    transactionContextDefaultValue.transaction
   );
-  const [chainTo, setChainTo] = useState<Chain>(transactionContextDefaultValue.transaction.chainTo);
-  const [token, setToken] = useState<Token>(transactionContextDefaultValue.transaction.token);
-  const [amount, setAmount] = useState(transactionContextDefaultValue.transaction.amount);
 
-  const setTransaction = useCallback((transaction: Transaction) => {
-    setChainFrom(transaction.chainFrom);
-    setChainTo(transaction.chainTo);
-    setToken(transaction.token);
-    setAmount(transaction.amount);
-  }, []);
   const value = useMemo(() => {
-    const transaction = { chainFrom, chainTo, token, amount };
     return { transaction, setTransaction };
-  }, [chainFrom, chainTo, token, amount, setTransaction]);
+  }, [transaction]);
 
   return <transactionContext.Provider value={value} {...props} />;
 };
