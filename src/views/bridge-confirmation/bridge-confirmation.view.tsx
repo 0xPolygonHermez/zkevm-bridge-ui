c17cd7e2cad8449893c3a1df457186415816681a
Alex Monso <amonsosanz@gmail.com>
2022-05-27 13:34:57 +0200

    Add the fiatAmount to the bridge confirmation view

--- src/views/bridge-confirmation/bridge-confirmation.view.tsx
+++ src/views/bridge-confirmation/bridge-confirmation.view.tsx
@@ -4,15 +4,12 @@ import { useNavigate } from "react-router-dom";
 import { ReactComponent as ArrowRightIcon } from "src/assets/icons/arrow-right.svg";
 import useBridgeConfirmationStyles from "src/views/bridge-confirmation/bridge-confirmation.styles";
 import Header from "src/views/shared/header/header.view";
-import { useFormContext } from "src/contexts/form.context";
 import Card from "src/views/shared/card/card.view";
 import Typography from "src/views/shared/typography/typography.view";
 import routes from "src/routes";
 import Button from "src/views/shared/button/button.view";
 import Error from "src/views/shared/error/error.view";
-import { useProvidersContext } from "src/contexts/providers.context";
 import Icon from "src/views/shared/icon/icon.view";
-import { useBridgeContext } from "src/contexts/bridge.context";
 import { getChainName } from "src/utils/labels";
 import { formatTokenAmount } from "src/utils/amounts";
 import {
@@ -20,8 +17,13 @@ import {
   isMetamaskInsufficientAllowanceError,
 } from "src/utils/types";
 import { parseError } from "src/adapters/error";
-import { useErrorContext } from "src/contexts/error.context";
+import { getCurrency } from "src/adapters/storage";
+import { useBridgeContext } from "src/contexts/bridge.context";
 import { useEnvContext } from "src/contexts/env.context";
+import { useErrorContext } from "src/contexts/error.context";
+import { useFormContext } from "src/contexts/form.context";
+import { useProvidersContext } from "src/contexts/providers.context";
+import { usePriceOracleContext } from "src/contexts/price-oracle.context";
 import { ETH_TOKEN_LOGO_URI } from "src/constants";
 
 const BridgeConfirmation: FC = () => {
@@ -32,7 +34,10 @@ const BridgeConfirmation: FC = () => {
   const { bridge } = useBridgeContext();
   const { formData, setFormData } = useFormContext();
   const { account, connectedProvider } = useProvidersContext();
+  const { getTokenPrice } = usePriceOracleContext();
+  const [fiatAmount, setFiatAmount] = useState<number>();
   const [error, setError] = useState<string>();
+  const preferredCurrency = getCurrency();
 
   const onClick = () => {
     if (formData && account.status === "successful") {
@@ -83,6 +88,12 @@ const BridgeConfirmation: FC = () => {
     }
   }, [navigate, formData]);
 
+  useEffect(() => {
+    if (formData) {
+      getTokenPrice(formData.token).then(setFiatAmount).catch(notifyError);
+    }
+  }, [formData, getTokenPrice, notifyError]);
+
   if (!formData || !env) {
     return null;
   }
@@ -95,6 +106,7 @@ const BridgeConfirmation: FC = () => {
         <Typography type="h1">
           {`${formatTokenAmount(formData.amount, formData.token)} ${formData.token.symbol}`}
         </Typography>
+        {fiatAmount && <Typography type="h2">{`${preferredCurrency} ${fiatAmount}`}</Typography>}
         <div className={classes.chainsRow}>
           <div className={classes.chainBox}>
             <formData.from.Icon /> {getChainName(formData.from)}
