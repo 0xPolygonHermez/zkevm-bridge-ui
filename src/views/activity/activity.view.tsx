e126c3d78dfea5cbfec99995ae6033b3b35cb338
Alex Monso <amonsosanz@gmail.com>
2022-06-10 11:34:59 +0200

    Show a page loader and error on the activity component

--- src/views/activity/activity.view.tsx
+++ src/views/activity/activity.view.tsx
@@ -4,12 +4,14 @@ import BridgeCard from "src/views/activity/components/bridge-card/bridge-card.vi
 import useActivityStyles from "src/views/activity/activity.styles";
 import Typography from "src/views/shared/typography/typography.view";
 import Header from "src/views/shared/header/header.view";
+import PageLoader from "src/views/shared/page-loader/page-loader.view";
+import Error from "src/views/shared/error/error.view";
 import { useBridgeContext } from "src/contexts/bridge.context";
 import { useProvidersContext } from "src/contexts/providers.context";
 import { useEnvContext } from "src/contexts/env.context";
 import { useErrorContext } from "src/contexts/error.context";
 import { parseError } from "src/adapters/error";
-import { isMetamaskUserRejectedRequestError } from "src/utils/types";
+import { AsyncTask, isMetamaskUserRejectedRequestError } from "src/utils/types";
 import { AUTO_REFRESH_RATE } from "src/constants";
 import { Bridge } from "src/domain";
 import useCallIfMounted from "src/hooks/use-call-if-mounted";
@@ -20,13 +22,10 @@ const Activity: FC = () => {
   const { getBridges, claim } = useBridgeContext();
   const { account, connectedProvider } = useProvidersContext();
   const { notifyError } = useErrorContext();
-  const [bridgeList, setBridgeList] = useState<Bridge[]>([]);
+  const [bridgeList, setBridgeList] = useState<AsyncTask<Bridge[], string>>({ status: "pending" });
   const [displayAll, setDisplayAll] = useState(true);
   const [wrongNetworkBridges, setWrongNetworkBridges] = useState<Bridge["id"][]>([]);
   const classes = useActivityStyles({ displayAll });
-
-  const pendingBridges = bridgeList.filter((bridge) => bridge.status !== "completed");
-  const filteredList = displayAll ? bridgeList : pendingBridges;
 
   const onDisplayAll = () => setDisplayAll(true);
   const onDisplayPending = () => setDisplayAll(false);
@@ -61,11 +60,15 @@ const Activity: FC = () => {
         getBridges({ env, ethereumAddress: account.data })
           .then((bridges) => {
             callIfMounted(() => {
-              setBridgeList(bridges);
+              setBridgeList({ status: "successful", data: bridges });
             });
           })
           .catch((error) => {
             callIfMounted(() => {
+              setBridgeList({
+                status: "failed",
+                error: "Something is wrong, we couldn't load the Bridges",
+              });
               notifyError(error);
             });
           });
@@ -83,37 +86,60 @@ const Activity: FC = () => {
     setWrongNetworkBridges([]);
   }, [connectedProvider?.chainId]);
 
-  return (
-    <>
-      <Header title="Activity" backTo="home" />
-      <div className={classes.selectorBoxes}>
-        <div className={`${classes.selectorBox} ${classes.allBox}`} onClick={onDisplayAll}>
-          <Typography type="body1" className={classes.status}>
-            All
-          </Typography>
-          <Typography type="body2" className={classes.numberAllBox}>
-            {bridgeList.length}
-          </Typography>
-        </div>
-        <div className={`${classes.selectorBox} ${classes.pendingBox}`} onClick={onDisplayPending}>
-          <Typography type="body1" className={classes.status}>
-            Pending
-          </Typography>
-          <Typography type="body2" className={classes.numberPendingBox}>
-            {pendingBridges.length}
-          </Typography>
-        </div>
-      </div>
-      {filteredList.map((bridge) => (
-        <BridgeCard
-          bridge={bridge}
-          onClaim={() => onClaim(bridge)}
-          networkError={wrongNetworkBridges.includes(bridge.id)}
-          key={bridge.id}
-        />
-      ))}
-    </>
-  );
+  switch (bridgeList.status) {
+    case "pending":
+    case "loading":
+    case "reloading": {
+      return <PageLoader />;
+    }
+    case "failed": {
+      return (
+        <>
+          <Header title="Activity" backTo="home" />
+          <Error error={bridgeList.error} />
+        </>
+      );
+    }
+    case "successful": {
+      const pendingBridges = bridgeList.data.filter((bridge) => bridge.status !== "completed");
+      const filteredList = displayAll ? bridgeList.data : pendingBridges;
+
+      return (
+        <>
+          <Header title="Activity" backTo="home" />
+          <div className={classes.selectorBoxes}>
+            <div className={`${classes.selectorBox} ${classes.allBox}`} onClick={onDisplayAll}>
+              <Typography type="body1" className={classes.status}>
+                All
+              </Typography>
+              <Typography type="body2" className={classes.numberAllBox}>
+                {bridgeList.data.length}
+              </Typography>
+            </div>
+            <div
+              className={`${classes.selectorBox} ${classes.pendingBox}`}
+              onClick={onDisplayPending}
+            >
+              <Typography type="body1" className={classes.status}>
+                Pending
+              </Typography>
+              <Typography type="body2" className={classes.numberPendingBox}>
+                {pendingBridges.length}
+              </Typography>
+            </div>
+          </div>
+          {filteredList.map((bridge) => (
+            <BridgeCard
+              bridge={bridge}
+              onClaim={() => onClaim(bridge)}
+              networkError={wrongNetworkBridges.includes(bridge.id)}
+              key={bridge.id}
+            />
+          ))}
+        </>
+      );
+    }
+  }
 };
 
 export default Activity;
