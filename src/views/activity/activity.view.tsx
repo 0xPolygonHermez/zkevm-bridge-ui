3407485d7c9a54209daaffa42c59ec8b6b5bd024
Alex Monso <amonsosanz@gmail.com>
2022-06-15 14:10:58 +0200

    Implement infinite scrolling on the activity view (polling has been disabled)

--- src/views/activity/activity.view.tsx
+++ src/views/activity/activity.view.tsx
@@ -1,5 +1,6 @@
 import { useState, FC, useEffect } from "react";
 
+import InfiniteScroll from "src/views/activity/components/infinite-scroll/infinite-scroll.view";
 import BridgeCard from "src/views/activity/components/bridge-card/bridge-card.view";
 import useActivityStyles from "src/views/activity/activity.styles";
 import Typography from "src/views/shared/typography/typography.view";
@@ -24,6 +25,8 @@ const Activity: FC = () => {
   const { notifyError } = useErrorContext();
   const [bridgeList, setBridgeList] = useState<AsyncTask<Bridge[], string>>({ status: "pending" });
   const [displayAll, setDisplayAll] = useState(true);
+  const [nextFromItem, setNextFromItem] = useState(PAGE_SIZE);
+  const [endReached, setEndReached] = useState(false);
   const [wrongNetworkBridges, setWrongNetworkBridges] = useState<Bridge["id"][]>([]);
   const classes = useActivityStyles({ displayAll });
 
@@ -73,77 +76,119 @@ const Activity: FC = () => {
             });
           });
       };
-      const intervalId = setInterval(loadBridges, AUTO_REFRESH_RATE);
+      // const intervalId = setInterval(loadBridges, AUTO_REFRESH_RATE);
       loadBridges();
 
-      return () => {
-        clearInterval(intervalId);
-      };
+      // return () => {
+      //   clearInterval(intervalId);
+      // };
     }
   }, [account, env, getBridges, notifyError, callIfMounted]);
 
   useEffect(() => {
     setWrongNetworkBridges([]);
   }, [connectedProvider?.chainId]);
-  switch (bridgeList.status) {
-    case "pending":
-    case "loading":
-    case "reloading": {
-      return (
-        <>
-          <Header title="Activity" backTo="home" />
-          <PageLoader />
-        </>
-      );
-    }
-    case "failed": {
-      return (
-        <>
-          <Header title="Activity" backTo="home" />
-          <Error error={bridgeList.error} />
-        </>
-      );
-    }
-    case "successful": {
-      const pendingBridges = bridgeList.data.filter((bridge) => bridge.status !== "completed");
-      const filteredList = displayAll ? bridgeList.data : pendingBridges;
 
-      return (
-        <>
-          <Header title="Activity" backTo="home" />
-          <div className={classes.selectorBoxes}>
-            <div className={`${classes.selectorBox} ${classes.allBox}`} onClick={onDisplayAll}>
-              <Typography type="body1" className={classes.status}>
-                All
-              </Typography>
-              <Typography type="body2" className={classes.numberAllBox}>
-                {bridgeList.data.length}
-              </Typography>
-            </div>
-            <div
-              className={`${classes.selectorBox} ${classes.pendingBox}`}
-              onClick={onDisplayPending}
-            >
-              <Typography type="body1" className={classes.status}>
-                Pending
-              </Typography>
-              <Typography type="body2" className={classes.numberPendingBox}>
-                {pendingBridges.length}
-              </Typography>
-            </div>
-          </div>
-          {filteredList.map((bridge) => (
-            <BridgeCard
-              bridge={bridge}
-              onClaim={() => onClaim(bridge)}
-              networkError={wrongNetworkBridges.includes(bridge.id)}
-              key={bridge.id}
-            />
-          ))}
-        </>
-      );
-    }
-  }
+  return (
+    <InfiniteScroll
+      asyncTaskStatus={bridgeList.status}
+      fromItem={nextFromItem}
+      onLoadNextPage={(fromItem) => {
+        if (
+          env &&
+          account.status === "successful" &&
+          bridgeList.status === "successful" &&
+          endReached === false
+        ) {
+          setBridgeList({ status: "reloading", data: bridgeList.data });
+          setNextFromItem(fromItem + PAGE_SIZE);
+          getBridges({ env, ethereumAddress: account.data, limit: PAGE_SIZE, offset: fromItem })
+            .then((bridges) => {
+              callIfMounted(() => {
+                setEndReached(bridges.length < PAGE_SIZE);
+                setBridgeList({ status: "successful", data: [...bridgeList.data, ...bridges] });
+              });
+            })
+            .catch((error) => {
+              callIfMounted(() => {
+                setBridgeList({
+                  status: "failed",
+                  error: "Something is wrong, we couldn't load the Bridges",
+                });
+                notifyError(error);
+              });
+            });
+        }
+      }}
+    >
+      {(() => {
+        switch (bridgeList.status) {
+          case "pending":
+          case "loading": {
+            return (
+              <>
+                <Header title="Activity" backTo="home" />
+                <PageLoader />
+              </>
+            );
+          }
+          case "failed": {
+            return (
+              <>
+                <Header title="Activity" backTo="home" />
+                <Error error={bridgeList.error} />
+              </>
+            );
+          }
+          case "successful":
+          case "reloading": {
+            const pendingBridges = bridgeList.data.filter(
+              (bridge) => bridge.status !== "completed"
+            );
+            const filteredList = displayAll ? bridgeList.data : pendingBridges;
+
+            return (
+              <>
+                <Header title="Activity" backTo="home" />
+                <div className={classes.selectorBoxes}>
+                  <div
+                    className={`${classes.selectorBox} ${classes.allBox}`}
+                    onClick={onDisplayAll}
+                  >
+                    <Typography type="body1" className={classes.status}>
+                      All
+                    </Typography>
+                    <Typography type="body2" className={classes.numberAllBox}>
+                      {bridgeList.data.length}
+                    </Typography>
+                  </div>
+                  <div
+                    className={`${classes.selectorBox} ${classes.pendingBox}`}
+                    onClick={onDisplayPending}
+                  >
+                    <Typography type="body1" className={classes.status}>
+                      Pending
+                    </Typography>
+                    <Typography type="body2" className={classes.numberPendingBox}>
+                      {pendingBridges.length}
+                    </Typography>
+                  </div>
+                </div>
+                {filteredList.map((bridge) => (
+                  <BridgeCard
+                    bridge={bridge}
+                    onClaim={() => onClaim(bridge)}
+                    networkError={wrongNetworkBridges.includes(bridge.id)}
+                    key={bridge.id}
+                  />
+                ))}
+              </>
+            );
+          }
+        }
+      })()}
+    </InfiniteScroll>
+  );
 };
 
 export default Activity;
