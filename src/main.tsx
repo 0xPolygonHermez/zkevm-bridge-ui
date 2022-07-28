750cfa0136184ca21a580741bb9c860c4a913a66
Elias Garcia <eliasgarciadev@gmail.com>
2022-07-28 12:21:06 +0200

    Add vite base config

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "react-jss";
 import { BrowserRouter } from "react-router-dom";
 import "normalize.css/normalize.css";
@@ -7,13 +7,12 @@ import "normalize.css/normalize.css";
 import theme from "src/styles/theme";
 import App from "src/views/app.view";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("app-root")
+  </React.StrictMode>
 );
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="vite/client" />
 /// <reference types="react-scripts" />
 
 import { ExternalProvider, TransactionResponse } from "@ethersproject/providers";
