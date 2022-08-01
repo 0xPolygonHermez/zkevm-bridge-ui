9cd88a342fb2f01e002cd53b8a0e3402805577fd
Elias Garcia <eliasgarciadev@gmail.com>
2022-08-01 18:15:28 +0200

    Add type checking to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import checker from "vite-plugin-checker";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: "eslint --ext .ts,.tsx .",
+        dev: { logLevel: ["error"] },
+      },
+    }),
+  ],
   resolve: {
     alias: [{ find: "src", replacement: path.resolve(__dirname, "src") }],
   },
-  define: { global: "window"},
+  define: { global: "window" },
   build: {
     rollupOptions: {
-      external: ["jss-plugin-window"]
-    }
-  }
+      external: ["jss-plugin-window"],
+    },
+  },
 });
