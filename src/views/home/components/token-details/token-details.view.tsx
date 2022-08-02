1591f36e6024c3c3a58e013060a417689219d262
Alex Monso <amonsosanz@gmail.com>
2022-08-02 18:06:24 +0200

    Manage the ethereum native token in the token details screen

--- src/views/home/components/token-details/token-details.view.tsx
+++ src/views/home/components/token-details/token-details.view.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { constants as ethersConstants } from "ethers";
 
 import useTokenDetailsStyles from "src/views/home/components/token-details/token-details.styles";
 import Typography from "src/views/shared/typography/typography.view";
@@ -19,72 +20,123 @@ const TokenDetails: FC<TokenDetailsProps> = ({ token, className }) => {
     return null;
   }
 
-  const nativeTokenChain = env.chains.find(({ chainId }) => chainId === token.chainId);
-  const nativeTokenAddress = token.address;
-
-  const wrappedTokenChainId = token.wrappedToken?.chainId;
-  const wrappedTokenAddress = token.wrappedToken?.address;
-
-  const wrappedTokenChain =
-    wrappedTokenChainId !== undefined
-      ? env.chains.find(({ chainId }) => chainId === wrappedTokenChainId)
-      : undefined;
-
-  const nativeAddressRow = nativeTokenChain ? (
+  const nameRow = (
     <div className={classes.row}>
       <Typography type="body2" className={classes.alignRow}>
-        <nativeTokenChain.Icon />
-        {`${nativeTokenChain.key === "ethereum" ? "L1" : "L2"} token address`}
+        Token name
       </Typography>
       <Typography type="body1" className={classes.alignRow}>
-        {getPartiallyHiddenEthereumAddress(nativeTokenAddress)}
+        {token.name}
       </Typography>
     </div>
-  ) : null;
+  );
 
-  const wrappedAddressRow =
-    wrappedTokenChain && wrappedTokenAddress ? (
+  const symbolRow = (
+    <div className={classes.row}>
+      <Typography type="body2" className={classes.alignRow}>
+        Token symbol
+      </Typography>
+      <Typography type="body1" className={classes.alignRow}>
+        {token.symbol}
+      </Typography>
+    </div>
+  );
+
+  const decimalsRow = (
+    <div className={classes.row}>
+      <Typography type="body2" className={classes.alignRow}>
+        Token decimals
+      </Typography>
+      <Typography type="body1" className={classes.alignRow}>
+        {token.decimals}
+      </Typography>
+    </div>
+  );
+
+  if (token.address === ethersConstants.AddressZero) {
+    const ethereum = env.chains[0];
+    const polygonZkEVM = env.chains[1];
+
+    const ethereumRow = (
       <div className={classes.row}>
         <Typography type="body2" className={classes.alignRow}>
-          <wrappedTokenChain.Icon />
-          {`${wrappedTokenChain.key === "ethereum" ? "L1" : "L2"} token address`}
+          <ethereum.Icon />
+          L1 token address
         </Typography>
         <Typography type="body1" className={classes.alignRow}>
-          {getPartiallyHiddenEthereumAddress(wrappedTokenAddress)}
+          {getPartiallyHiddenEthereumAddress(ethersConstants.AddressZero)}
+        </Typography>
+      </div>
+    );
+
+    const polygonZkEVMRow = (
+      <div className={classes.row}>
+        <Typography type="body2" className={classes.alignRow}>
+          <polygonZkEVM.Icon />
+          L2 token address
+        </Typography>
+        <Typography type="body1" className={classes.alignRow}>
+          {getPartiallyHiddenEthereumAddress(ethersConstants.AddressZero)}
+        </Typography>
+      </div>
+    );
+
+    return (
+      <div className={`${classes.wrapper} ${className || ""}`}>
+        {ethereumRow}
+        {polygonZkEVMRow}
+        {nameRow}
+        {symbolRow}
+        {decimalsRow}
+      </div>
+    );
+  } else {
+    const nativeTokenChain = env.chains.find(({ chainId }) => chainId === token.chainId);
+    const nativeTokenAddress = token.address;
+
+    const wrappedTokenChainId = token.wrappedToken?.chainId;
+    const wrappedTokenAddress = token.wrappedToken?.address;
+
+    const wrappedTokenChain =
+      wrappedTokenChainId !== undefined
+        ? env.chains.find(({ chainId }) => chainId === wrappedTokenChainId)
+        : undefined;
+
+    const nativeAddressRow = nativeTokenChain ? (
+      <div className={classes.row}>
+        <Typography type="body2" className={classes.alignRow}>
+          <nativeTokenChain.Icon />
+          {`${nativeTokenChain.key === "ethereum" ? "L1" : "L2"} token address`}
+        </Typography>
+        <Typography type="body1" className={classes.alignRow}>
+          {getPartiallyHiddenEthereumAddress(nativeTokenAddress)}
         </Typography>
       </div>
     ) : null;
 
-  return (
-    <div className={`${classes.wrapper} ${className || ""}`}>
-      {nativeAddressRow}
-      {wrappedAddressRow}
-      <div className={classes.row}>
-        <Typography type="body2" className={classes.alignRow}>
-          Token name
-        </Typography>
-        <Typography type="body1" className={classes.alignRow}>
-          {token.name}
-        </Typography>
+    const wrappedAddressRow =
+      wrappedTokenChain && wrappedTokenAddress ? (
+        <div className={classes.row}>
+          <Typography type="body2" className={classes.alignRow}>
+            <wrappedTokenChain.Icon />
+            {`${wrappedTokenChain.key === "ethereum" ? "L1" : "L2"} token address`}
+          </Typography>
+          <Typography type="body1" className={classes.alignRow}>
+            {getPartiallyHiddenEthereumAddress(wrappedTokenAddress)}
+          </Typography>
+        </div>
+      ) : null;
+
+    return (
+      <div className={`${classes.wrapper} ${className || ""}`}>
+        {nativeAddressRow}
+        {wrappedAddressRow}
+        {nameRow}
+        {symbolRow}
+        {decimalsRow}
       </div>
-      <div className={classes.row}>
-        <Typography type="body2" className={classes.alignRow}>
-          Token symbol
-        </Typography>
-        <Typography type="body1" className={classes.alignRow}>
-          {token.symbol}
-        </Typography>
-      </div>
-      <div className={classes.row}>
-        <Typography type="body2" className={classes.alignRow}>
-          Token decimals
-        </Typography>
-        <Typography type="body1" className={classes.alignRow}>
-          {token.decimals}
-        </Typography>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default TokenDetails;
