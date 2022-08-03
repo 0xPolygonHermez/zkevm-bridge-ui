aa980170fa433177eadc8c435d85ebe911273552
Alex Monso <amonsosanz@gmail.com>
2022-08-03 09:17:41 +0200

    Remove spaces of partially hidden addresses

--- src/utils/addresses.ts
+++ src/utils/addresses.ts
@@ -2,7 +2,7 @@ const getPartiallyHiddenEthereumAddress = (ethereumAddress: string): string => {
   const firstAddressSlice = ethereumAddress.slice(0, 6);
   const secondAddressSlice = ethereumAddress.slice(ethereumAddress.length - 4);
 
-  return `${firstAddressSlice} ･･･ ${secondAddressSlice}`;
+  return `${firstAddressSlice}･･･${secondAddressSlice}`;
 };
 
 export { getPartiallyHiddenEthereumAddress };
