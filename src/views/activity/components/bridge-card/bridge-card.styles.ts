24e8483684c9dfe084b31d217c262ba75fa94724
Elias Garcia <eliasgarciadev@gmail.com>
2022-08-10 17:06:44 +0200

    Add transition to the BridgeCard

--- src/views/activity/components/bridge-card/bridge-card.styles.ts
+++ src/views/activity/components/bridge-card/bridge-card.styles.ts
@@ -8,6 +8,7 @@ const useBridgeCardStyles = createUseStyles((theme: Theme) => ({
     padding: [theme.spacing(2), theme.spacing(3)],
     margin: [theme.spacing(2), "auto"],
     cursor: "pointer",
+    transition: theme.hoverTransition,
     "&:hover": {
       backgroundColor: theme.palette.grey.main,
     },
