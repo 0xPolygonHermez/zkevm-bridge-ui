0ad498ed9377072357b07724d8da5b27fc0b32dd
Elias Garcia <eliasgarciadev@gmail.com>
2022-09-30 00:38:12 +0200

    Improve the report button in the snackbar

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,11 +6,12 @@ const theme = {
       dark: "#5a1cc3",
     },
     white: "#ffffff",
-    black: "#081132",
+    black: "#0a0b0d",
     grey: {
       light: "#f0f1f6",
       main: "#e2e5ee",
       dark: "#7c7e96",
+      veryDark: "#363740",
     },
     warning: {
       main: "#e17e26",
