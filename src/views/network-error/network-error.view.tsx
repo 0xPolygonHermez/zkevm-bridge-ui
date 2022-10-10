bc5235d3afdcab844a6b185ed930be93ecbdc73b
Alex Monso <amonsosanz@gmail.com>
2022-10-10 17:10:46 +0200

    Avoid showing the network error page whitout errors

--- src/views/network-error/network-error.view.tsx
+++ src/views/network-error/network-error.view.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, Navigate } from "react-router-dom";
 
 import useNetworkErrorStyles from "src/views/network-error/network-error.styles";
 import { ReactComponent as PolygonZkEVMLogo } from "src/assets/polygon-zkevm-logo.svg";
@@ -12,29 +12,26 @@ const NetworkError: FC = () => {
   const navigate = useNavigate();
   const { state } = useLocation();
 
-  const parsedProviderError = providerError.nullable().safeParse(state);
+  const parsedProviderError = providerError.safeParse(state);
 
-  const getProviderError = () => {
-    if (parsedProviderError.success && parsedProviderError.data !== null) {
-      return parsedProviderError.data === ProviderError.Ethereum
-        ? "We cannot connect to the Ethereum node."
-        : "We cannot connect to the Polygon zkEVM node.";
-    }
-    return "We cannot connect to the node on L1 or L2.";
-  };
-
-  return (
+  return parsedProviderError.success ? (
     <div className={classes.wrapper}>
       <PolygonZkEVMLogo className={classes.logo} />
       <div className={classes.textBox}>
         <Typography type="h1">Network Error</Typography>
-        <Typography type="body1">{getProviderError()}</Typography>
+        <Typography type="body1">
+          {parsedProviderError.data === ProviderError.Ethereum
+            ? "We cannot connect to the Ethereum node."
+            : "We cannot connect to the Polygon zkEVM node."}
+        </Typography>
         <Typography type="body2">It will be operative again soon</Typography>
       </div>
-      <button type="button" onClick={() => navigate(routes.login.path)} className={classes.button}>
+      <button type="button" onClick={() => navigate(routes.home.path)} className={classes.button}>
         Try again
       </button>
     </div>
+  ) : (
+    <Navigate to={routes.home.path} />
   );
 };
 
