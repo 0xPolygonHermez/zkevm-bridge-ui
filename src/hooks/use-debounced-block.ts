744e687c59561806a6f758ebdfdf5d3a638ffc0b
Elias Garcia <eliasgarciadev@gmail.com>
2022-10-26 12:42:50 +0200

    Change useDebouncedBlock implementation

--- src/hooks/use-debounced-block.ts
+++ src/hooks/use-debounced-block.ts
@@ -1,61 +1,30 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
+import { useDebounce } from "src/hooks/use-debounce";
+import { AsyncTask, isAsyncTaskDataAvailable } from "src/utils/types";
 import { ConnectedProvider } from "src/domain";
-import { AsyncTask } from "src/utils/types";
-
-type DebouncedBlockCallback = (blockNumber: number) => void;
 
 const DEBOUNCE_TIME_IN_MS = 750;
 
 const useDebouncedBlock = (connectedProvider: AsyncTask<ConnectedProvider, string>) => {
-  const [listener, setListener] = useState<DebouncedBlockCallback>();
   const [blockNumber, setBlockNumber] = useState<number>();
 
-  const addBlockListener = useCallback(
-    (cb: DebouncedBlockCallback) => {
-      if (!listener) {
-        setListener(() => cb);
-      }
-    },
-    [listener]
-  );
-
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      if (listener && blockNumber) {
-        listener(blockNumber);
-      }
-    }, DEBOUNCE_TIME_IN_MS);
-
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, [blockNumber, listener]);
-
-  useEffect(() => {
-    if (listener) {
-      const onBlock = (newBlockNumber: number) => {
-        if (!blockNumber || newBlockNumber > blockNumber) {
-          setBlockNumber(newBlockNumber);
-        }
-      };
-
-      if (connectedProvider.status === "successful") {
-        void connectedProvider.data.provider.getBlockNumber().then((currentBlockNumber) => {
-          onBlock(currentBlockNumber);
-          connectedProvider.data.provider.on("block", onBlock);
+    if (isAsyncTaskDataAvailable(connectedProvider)) {
+      void connectedProvider.data.provider
+        .getBlockNumber()
+        .then(setBlockNumber)
+        .then(() => {
+          connectedProvider.data.provider.on("block", setBlockNumber);
         });
-      }
 
       return () => {
-        if (connectedProvider.status === "successful") {
-          connectedProvider.data.provider.off("block", onBlock);
-        }
+        connectedProvider.data.provider.off("block", setBlockNumber);
       };
     }
-  }, [blockNumber, connectedProvider, listener]);
+  }, [connectedProvider]);
 
-  return addBlockListener;
+  return useDebounce(blockNumber, DEBOUNCE_TIME_IN_MS);
 };
 
 export default useDebouncedBlock;
