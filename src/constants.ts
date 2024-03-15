e5d21095ccf4e848b2571da93ac725d261b77576
asolovov <andre.solovov@gmail.com>
2024-03-15 22:17:37 +0300

    fix: net name

--- src/constants.ts
+++ src/constants.ts
@@ -130,7 +130,7 @@ export const getChains = ({
       explorerUrl: polygonZkEVM.explorerUrl,
       Icon: PolygonZkEVMChainIcon,
       key: "polygon-zkevm",
-      name: polygonZkEVMNetworkName,
+      name: "Skopje Testnet",
       nativeCurrency: {
         decimals: 18,
         name: "SkpGPT",
